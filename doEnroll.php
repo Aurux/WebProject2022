16fa8d66beb26386d11438f1542c9515d03765de
Aurux <benjamin.wilson2@icloud.com>
2022-01-29 20:16:54 +0000

    Changed form inputs.

--- doEnroll.php
+++ doEnroll.php
@@ -1,7 +1,18 @@
+<?php include 'database.php'; ?>
+<?php session_start(); ?>
 <?php
 
+    $fn = $_POST['frmForename'];
+    $sn = $_POST['frmSurname'];
+    $gen = $_POST['frmGender'];
+    $dob = $_POST['frmDateOfBirth'];
+    $cs = $_POST['frmCourse'];
+    $pw = $_POST['frmPassword'];
+    $em = $_POST['frmEmail'];
+
+    $sql = "INSERT INTO user (forename, surname, gender, dob, course, email, password)
+            VALUES ('$fn', '$sn', '$gen', '$dob', '$cs', '$em', '$pw',)";
     
-
-
+    mysqli_query ($mysqli, $sql) or die (mysqli_error($mysqli));
 
     ?>
