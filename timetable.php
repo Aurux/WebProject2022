7f8495386a8d5c1c7a99add293d47c986869f43b
Daniel Glancy <20210944@gmail.com>
2022-02-10 16:18:21 +0000

    timetable

--- timetable.php
+++ timetable.php
@@ -20,4 +20,230 @@
             <li><a href="assessments.php">Assessments</a></li>
             <li><a class="active" href="timetable.php">Timetable</a></li>
         </ul>
+        
+    <TABLE class="myTimetable"> 
+        <h1>My Timetable</h1>
+    <THEAD>
+    <TR>
+      <TH></TH>
+      <TH>Monday</TH>
+      <TH>Tuesday</TH>
+      <TH>Wednesday</TH>
+      <TH>Thursday</TH>
+      <TH>Friday</TH>     
+    </TR>
+    </THEAD>
+    <TBODY>
+        <TR>
+            <TD>9:00</TD>
+            <TD>
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>     
+        </TR>
+        <TR>
+            <TD>10:00</TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+        </TR>
+        <TR>
+            <TD>11:00</TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+        </TR>
+        <TR>
+            <TD>12:00</TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+        </TR>
+        <TR>
+            <TD>13:00</TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+        </TR>
+        <TR>
+            <TD>14:00</TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+        </TR>
+        <TR>
+            <TD>15:00</TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+        </TR>
+        <TR>
+            <TD>16:00</TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+        </TR>
+        <TR>
+            <TD>17:00</TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+            <TD>        
+              <div class="subject"></div>
+              <div class="room"></div>
+            </TD>
+        </TR>
+   </TBODY>
+   </TABLE>
+   </body>
 </html>
+
+  
