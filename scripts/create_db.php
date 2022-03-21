65f4f27908a0c8f3a012d4d2eff449d92356562a
Aurux <benjamin.wilson2@icloud.com>
2022-03-21 18:08:43 +0000

    Made script to create DB.

--- scripts/create_db.php
+++ scripts/create_db.php
@@ -7,7 +7,23 @@
     else echo "DB failed to be created: " . mysqli_error($conn);
 
     $sql = "USE bowling_DB";
-    mysqli_query($conn, $sql)
+    mysqli_query($conn, $sql);
+
+    $sql = "CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR(20) PRIMARY KEY,
+                forename VARCHAR(30) NOT NULL,
+                surname VARCHAR(50) NOT NULL,
+                type ENUM('student', 'tutor') NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                authorised TINYINT
+            )";
+
+    if (mysqli_query($conn, $sql)) echo "<p>TABLE users CREATED.</p>";
+	else echo "<p>TABLE users FAILED TO BE CREATED: " . mysqli_error($conn) . "</p>";
 
 
+
+
+
+    mysqli_close($conn);
 ?>
