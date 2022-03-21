1ed2f51c07fa3865cfab1005412529efccb86fa3
Daniel Glancy <20210944@gmail.com>
2022-03-21 17:29:51 +0000

    sync

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php include 'database.php';
+
+
+$sql = "INSERT INTO `timetable` (`Time`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`)
+        VALUES ('', 'Bowling 101\r\nLR01', 'Lane Analysis\r\nLR02', 'Bowl design\r\nLR01', 'Pin manufacture\r\nLR02', 'Lane Analysis\r\nLRO1')";
