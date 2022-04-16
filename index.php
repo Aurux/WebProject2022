a046bbbca497358ca82e24095c4b6e430dcfe730
Aurux <benjamin.wilson2@icloud.com>
2022-04-16 20:26:48 +0100

    Improve navigation.

--- index.php
+++ index.php
@@ -3,7 +3,19 @@
     include("includes/header.php");
     include("includes/body.php");
     include("includes/nav.php");
-    include("login.php");
+
+    if ($_SESSION["loggedIn"] == true){
+        if ($_SESSION["uType"] == "student") {
+            header("refresh:0; url=student_home.php");
+        }
+        if ($_SESSION["uType"] == "tutor") {
+            header("refresh:0; url=tutor_home.php");
+        }
+    }
+    else {
+        include("login.php");
+    }
+    
     
     include_once("includes/footer.php");
 
