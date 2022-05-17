2cef22965d6de2c7ff111e114f43151755833e45
Ben Wilson <benjamin.wilson2@icloud.com>
2022-05-17 15:58:53 +0100

    Footer formatting

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
-    At Bowling Univeristy, our lecturers don't go on strike - they teach you to strike.
-    background source: https://www.istockphoto.com/photo/bowling-balls-gm1306463755-397034897
+    At Bowling Univeristy, our lecturers don't go on strike - they teach you to strike.<br>
+    Background sourced from <a href="https://www.istockphoto.com/photo/bowling-balls-gm1306463755-397034897">iStock Photos</a>
 </footer>
