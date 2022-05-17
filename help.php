c73d3e14d3f71e989a5d3b02a7107bdad58b0875
Ben Wilson <benjamin.wilson2@icloud.com>
2022-05-17 15:59:03 +0100

    Add privacy policy

--- help.php
+++ help.php
@@ -2,12 +2,16 @@
     $title = "Help";
     require_once("includes/header.php");
     include_once("includes/nav.php");
-?><div class="container">
-<h1>Help page</h1><br><br>
+?>
+<br><br>
+<table id="choicesTable"><caption>Help page</caption><td>
+
 <P>For all enquires please contact us either by email or by our contact number as shown below.</P><br><br>
 
-<label>Email: <a href="">bowlingUniversity@gmail.com</a></label><br><br>
-<label>Phone number: <a href="">+44 1632 960816</a></label>
+<label>Email: <a href="mailto:enquires@bowlU.ac.uk">enquires@bowlU.ac.uk</a></label><br><br>
+<label>Phone number: <a href="">+44 1632 960816</a></label><br><br>
+<a href="privacyPolicy.php">View our privacy policy</a>
 
-</div>
+</td></table>
+
 <?php include("includes/footer.php")?>
---
+++ privacyPolicy.php
@@ -0,0 +1,65 @@
+<?php
+    $title = "Privacy Policy";
+    require_once("includes/header.php");
+    include_once("includes/nav.php");
+?>
+<br><br>
+<table id="choicesTable" style="text-align: left; margin-bottom: 50px;"><caption>Privacy Policy</caption><td>
+
+<h2>Information about the collection of your personal data </h2>
+<br>    
+The controller is:<br>
+<br>
+Bowling Univeristy<br>
+Univeristy Network Services<br>
+47 Bolin Alley<br>
+Strikington<br>
+BL10 6AZ<br>
+<br>
+Questions relating to data protection can be directed to our data protection officer by email at: <a href="mailto:privacy@bowlU.ac.uk">privacy@bowlU.ac.uk</a>
+<br><br>
+<h2>Your rights as a data subject </h2><br>
+<h3>Right of access</h3><br>
+You have the right to obtain information from us about the categories of data stored, the purpose of processing, the recipients of the data, the planned storage period and your rights regarding data protection. For personal data not collected directly from you, you have the right to be informed about the source of the data.<br>
+<br><h3>Right to rectification</h3><br>
+If the personal data we process is inaccurate or incomplete, you have the right to have your data rectified.<br><br>
+<h3>Right to erasure</h3><br>
+You have the right to request that we delete your personal data. In certain cases, however, your data cannot be deleted. For example, if your data is required for an active contract or for our accounting and statutory responsibilities.<br><br>
+<h3>Right to restriction of processing</h3><br>
+You can request that we restrict the processing of your data in the following cases:<br><br>
+<ul>
+    <li>You have disputed the accuracy of data and you want us to stop using it pending final verification</li>
+    <li>The processing is unlawful, but you would prefer to restrict processing rather than have the data erased</li>
+    <li>We no longer need and would otherwise delete the data, but you still need it for the assertion, exercise or defence of legal claims</li>
+    <li>You have lodged an objection to the data processing, which is still under review</li>
+</ul>
+<br><br><br><br><br><br><br><br>
+In the event of a restriction, we will ensure that the personal data cannot be further processed or changed by us. If processing has been restricted in accordance with the above conditions, the controller will inform you before lifting the restriction.<br><br>
+<h3>Right to data portability </h3><br>
+You have the right to receive the personal data that you provided to us in a structured, commonly used and machine-readable format and, if necessary, to have it transferred to third parties.
+
+The right to data portability shall not apply to processing that is necessary for the performance of a task carried out in the public interest or in the exercise of official authority vested in the controller.<br><br>
+<h3>Right to object</h3><br>
+You have the right to object at any time to the processing of personal data that we process on the basis of a "legitimate interest"; this also applies to profiling based on these provisions.
+
+We will then no longer process your personal data unless we can demonstrate compelling legitimate grounds for the processing that override your interests, rights and freedoms, or unless the processing serves to assert, exercise or defend legal claims.<br><br>
+To exercise your right to object, you must submit an objection to us, the data controller. You are welcome to address your data protection concerns (e.g. your objection) directly to the responsible contact person at <a href="mailto:privacy@bowlU.ac.uk">privacy@bowlU.ac.uk</a>.<br><br>
+<h3>Right to withdraw </h3><br>
+You have the right to withdraw your consent to future processing at any time.<br>
+Please send your notification of withdrawal to us at <a href="mailto:privacy@bowlU.ac.uk">privacy@bowlU.ac.uk</a>.<br>
+<br>
+If you would like to exercise any of the above rights, please write to us at <a href="mailto:privacy@bowlU.ac.uk">privacy@bowlU.ac.uk</a> and we will take care of your request immediately.<br>
+<br><h3>Right to lodge a complaint with a supervisory authority </h3><br>
+You also have the right to lodge a complaint with the supervisory authority responsible for data protection at any time.<br><br>
+Here you will find the contact information for the UK Information Commissioner's Office:<br><br>
+Information Commissioner's Office<br>
+Wycliffe House<br>
+Water Lane<br>
+Wilmslow<br>
+Cheshire<br>
+SK9 5AF<br>
+Tel: 0303 123 1113<br><br>
+
+</td></table>
+
+<?php include("includes/footer.php")?>
